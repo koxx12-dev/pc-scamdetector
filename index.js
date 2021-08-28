6e296717d93e92e66f00ad820038586072962e75
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2021-08-28 21:55:47 +0200

    maybe works?

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const { Plugin } = require("powercord/entities");
 const { FluxDispatcher } = require("powercord/webpack");
+const { clipboard } = require("electron");
 
 module.exports = class ScamDetector extends Plugin {
 
   onMessage(data) {
-    console.log(data)
-  }
-
-  startPlugin() {
-    FluxDispatcher.subscribe('MESSAGE_CREATE', this.onMessage)
-    powercord.api.commands.registerCommand({
-      command: "test",
-      description: "test",
-      usage: "{c}",
-      executor: (args) => {
+    try {
+      var message = data.message.content;
+      var authorId = data.message.author.id;
+      if (
+        message.includes("free") &&
+        message.includes("nitro") &&
+        message.includes("http")
+      ) {
         powercord.api.notices.sendToast("scam-decetector", {
           header: "Detected a scam url",
-          content: "Click copy to copy the content of the message",
+          content: 'Click "copy" to copy the content of the message',
           type: "danger",
           buttons: [
             {
               text: "Copy",
               color: "green",
               look: "ghost",
-              onClick: () => console.log("Test"),
+              onClick: () =>
+                clipboard.write({
+                  text: authorId+"|"+message,
+                }),
             },
             {
               text: "Ignore",
@@ -32,12 +34,15 @@ module.exports = class ScamDetector extends Plugin {
             },
           ],
         });
-      },
-    });
+      }
+    } catch (error) {}
+  }
+
+  startPlugin() {
+    FluxDispatcher.subscribe('MESSAGE_CREATE', this.onMessage)
   }
 
   pluginWillUnload() {
-    powercord.api.commands.unregisterCommand("test");
     FluxDispatcher.unsubscribe('MESSAGE_CREATE', this.onMessage)
   }
 };
