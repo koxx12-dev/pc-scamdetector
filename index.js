e647814778a9eecec5b354d92d8cc9667acf2762
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2021-08-29 12:34:42 +0200

    more stuff works

--- index.js
+++ index.js
@@ -7,45 +7,17 @@ const Settings = require("./settings.jsx");
 var userId = 0;
 
 module.exports = class ScamDetector extends Plugin {
-
-  async startPlugin() {
-    this.getCurrentUser = await getModule(
-      ["getCurrentUser"],
-      false
-    ).getCurrentUser();
-    
-    userId = this.getCurrentUser.id;
-
-    powercord.api.settings.registerSettings("pc-scamdetector", {
-      category: this.entityID,
-      label: "Scam Detector",
-      render: Settings,
-    });
-
-    FluxDispatcher.subscribe("MESSAGE_CREATE",(data) => this.onMessage(data,this.settings));
-  }
-
-  pluginWillUnload() {
-    FluxDispatcher.unsubscribe("MESSAGE_CREATE", this.onMessage);
-    powercord.api.settings.unregisterSettings("pc-scamdetector");
-  }
-
   getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  async onMessage(data,settings) {
-  
-    console.log(data)
-
+  async onMessage(data, settings) {
+    
     const toast = settings.get("toast", true);
     const cache = settings.get("cache", false);
 
-    console.log(toast);
-
-    console.log(userId);
     try {
       var message = data.message.content;
       var authorId = data.message.author.id;
@@ -87,5 +59,29 @@ module.exports = class ScamDetector extends Plugin {
         }
       }
     } catch (error) {}
+  }
+
+  async startPlugin() {
+    this.getCurrentUser = await getModule(
+      ["getCurrentUser"],
+      false
+    ).getCurrentUser();
+
+    userId = this.getCurrentUser.id;
+
+    powercord.api.settings.registerSettings("pc-scamdetector", {
+      category: this.entityID,
+      label: "Scam Detector",
+      render: Settings,
+    });
+
+    FluxDispatcher.subscribe("MESSAGE_CREATE", (data) =>
+      this.onMessage(data, this.settings)
+    );
+  }
+
+  pluginWillUnload() {
+    FluxDispatcher.unsubscribe("MESSAGE_CREATE", this.onMessage);
+    powercord.api.settings.unregisterSettings("pc-scamdetector");
   }
 };
